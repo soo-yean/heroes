55bb4e7bfe595d99775613a3279de9b6c01a9416
soo-yean <sooyean.jang@gmail.com>
2025-02-14 12:24:54 +0900

    ADD feature: update hero list from input

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,27 @@ import { HEROES } from "./data/heroes-dummy";
 import { Hero } from "./types/hero";
 
 function App() {
-  const heroes: Hero[] = HEROES;
-  const [hero, setHero] = useState<Hero>({ id: 1, name: "Wolverine" });
+  const [heroes, setHeroes] = useState<Hero[]>(HEROES);
+  const [currHeroId, setCurrHeroId] = useState<number | null>(null);
+
+  const currHero = heroes.find((hero) => hero.id === currHeroId);
 
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setHero({ ...hero, name: e.target.value }); //spread operator to create a new object, not update
+    const updatedName = e.target.value;
+
+    //spread operator to create a new object and replace the original, not update
+    setHeroes((prevHeroes) =>
+      prevHeroes.map((hero) => {
+        if (hero.id === currHeroId) {
+          return { ...hero, name: updatedName };
+        }
+        return hero;
+      })
+    );
+  };
+
+  const handleCurrHero = (id: number) => {
+    setCurrHeroId(id);
   };
 
   return (
@@ -21,7 +37,11 @@ function App() {
           equals
         () => () */}
         {heroes.map((hero) => (
-          <li key={hero.id} className="flex">
+          <li
+            onClick={() => handleCurrHero(hero.id)}
+            key={hero.id}
+            className="flex cursor-pointer"
+          >
             <span className="bg-blue-600 text-white rounded-l p-2">
               {hero.id}
             </span>
@@ -32,26 +52,30 @@ function App() {
         ))}
       </ul>
 
-      <h2 className="text-2xl">Details</h2>
-      <div>
-        <span className="font-bold">ID: </span>
-        <span>{hero.id}</span>
-        <div className="space-x-2">
-          <span className="font-bold">Name:</span>
-          <span className="uppercase">{hero.name}</span>
-        </div>
-      </div>
+      {currHero && (
+        <>
+          <h2 className="text-2xl">Details</h2>
+          <div>
+            <span className="font-bold">ID: </span>
+            <span>{currHero.id}</span>
+            <div className="space-x-2">
+              <span className="font-bold">Name:</span>
+              <span className="uppercase">{currHero.name}</span>
+            </div>
+          </div>
 
-      <div className="flex flex-col gap-2 mt-3 border-t">
-        <label className="mt-2">Hero Name</label>
-        <input
-          className="border border-gray-300 rounded-lg p-2 w-1/4"
-          type="text"
-          placeholder="name"
-          value={hero.name}
-          onChange={handleNameChange}
-        />
-      </div>
+          <div className="flex flex-col gap-2 mt-3 border-t">
+            <label className="mt-2">Hero Name</label>
+            <input
+              className="border border-gray-300 rounded-lg p-2 w-1/4"
+              type="text"
+              placeholder="name"
+              value={currHero.name}
+              onChange={handleNameChange}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
