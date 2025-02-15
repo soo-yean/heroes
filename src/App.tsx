b6e256a253fb2d3e48fd2ed973c2ecc269ad419d
soo-yean <sooyean.jang@gmail.com>
2025-02-15 15:20:44 +0900

    ADD feature: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,12 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { Hero } from "./types/hero";
-import HeroDetails from "./Components/HeroDetails";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [heroes, setHeroes] = useState<Hero[]>([]);
-  const [currHeroId, setCurrHeroId] = useState<number | null>(null);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/heroes")
-      .then((res) => res.json())
-      .then((data) => {
-        setHeroes(data);
-      });
-  }, []);
-
-  const currHero = heroes.find((hero) => hero.id === currHeroId);
-
-  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const updatedName = e.target.value;
-
-    //spread operator to create a new object and replace the original, not update
-    setHeroes((prevHeroes) =>
-      prevHeroes.map((hero) => {
-        if (hero.id === currHeroId) {
-          return { ...hero, name: updatedName };
-        }
-        return hero;
-      })
-    );
-  };
-
-  const handleCurrHero = (id: number) => {
-    setCurrHeroId(id);
-  };
-
   return (
-    <div className="container mt-5 mx-auto">
-      <h2 className="text-2xl">My heroes</h2>
-      <ul className="flex flex-col gap-2 my-3">
-        {/* 
-        () => {
-          return 
-        } 
-          equals
-        () => () */}
-        {heroes.map((hero) => (
-          <li
-            onClick={() => handleCurrHero(hero.id)}
-            key={hero.id}
-            className="flex cursor-pointer"
-          >
-            <span className="bg-blue-600 text-white rounded-l p-2">
-              {hero.id}
-            </span>
-            <span className="bg-yellow-300 rounded-r w-1/4 p-2">
-              {hero.name}
-            </span>
-          </li>
-        ))}
-      </ul>
-
-      <HeroDetails hero={currHero} onNameChange={handleNameChange} />
+    <div className="mt-5 container mx-auto flex justify-between gap-6">
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <div className="flex-1">Messages go here...</div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./Components/Dashboard.tsx";
+import HeroesList from "./Components/HeroesList.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          {/* Redirect to dashboard when the user visits the root */}
+          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/heroes" element={<HeroesList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
